01dbe32ab10342ebcf3bb21cf7987d79a8094942
Nancy-200507 <nancy2005sn@gmail.com>
2025-09-28 16:36:09 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 @echo off
 cd /d %~dp0
-java -cp lib/Ab.jar Main bot=super action=chat trace=false command="load aiml b"
-
+java -cp lib/Ab.jar Main bot=super action=chat trace=false path="C:/Users/Rahul Ganesh/Downloads/program-ab-0.0.4.3" command="load aiml b"
 
 
